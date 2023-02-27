1d13b162a3a69097edc20308bab6de29a70d676c
snwinz <stefan.winzinger@uni-bamberg.de>
2023-02-27 20:31:06 +0100

    Added additional selection for test targets

--- src/main/java/gui/view/SelectionDialogueOfTestTargets.java
+++ src/main/java/gui/view/SelectionDialogueOfTestTargets.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import logic.model.TestSuiteOfTargets;
@@ -19,15 +20,17 @@ public class SelectionDialogueOfTestTargets extends Stage {
 
     private final Spinner<Integer> minCounter;
     private final Spinner<Integer> maxCounter;
+    private final TextField testTargetName;
 
     public SelectionDialogueOfTestTargets(TestSuiteOfTargets testSuiteOfTargets, CriteriaSelectionDynamicTestCaseController controller) {
         this.testSuiteOfTargets = testSuiteOfTargets;
-        this.minCounter = new Spinner<>(1, testSuiteOfTargets.getTestTargets().size(), 1);
-        this.maxCounter = new Spinner<>(1, testSuiteOfTargets.getTestTargets().size(), 1);
+        this.minCounter = new Spinner<>(0, testSuiteOfTargets.getTestTargets().size(), 1);
+        this.maxCounter = new Spinner<>(0, testSuiteOfTargets.getTestTargets().size(), 1);
+        this.testTargetName = new TextField("");
         minCounter.setEditable(true);
         maxCounter.setEditable(true);
         this.controller = controller;
-        this.setTitle("Select Test Targets to be generated");
+        this.setTitle("Filter Test Targets to be generated");
         var grid = getGridPane();
         var scene = new Scene(grid);
         this.setScene(scene);
@@ -36,36 +39,58 @@ public class SelectionDialogueOfTestTargets extends Stage {
 
     private GridPane getGridPane() {
         GridPane gridPane = new GridPane();
-        var firstTestSuite = new Label("First Test Suite (included): ");
-        var lastTestSuite = new Label("Last Test Suite (included): ");
+        var firstTestSuite = new Label("First Test Target (included): ");
+        var lastTestSuite = new Label("Last Test Target (excluded): ");
+        var testTargetName = new Label("Name of logs for target: ");
 
 
         gridPane.add(firstTestSuite, 0, 0);
         gridPane.add(minCounter, 1, 0);
         gridPane.add(lastTestSuite, 0, 1);
         gridPane.add(maxCounter, 1, 1);
+        gridPane.add(testTargetName, 3, 0);
+        gridPane.add(this.testTargetName, 4, 0);
 
-        Button createButton = new Button("Create");
-        createButton.setDefaultButton(true);
-        createButton.setOnAction(getCreateButtonEvent());
+
+        Button numberFilterButton = new Button("Create");
+        numberFilterButton.setDefaultButton(true);
+        numberFilterButton.setOnAction(getNumberFilterButtonEvent());
         Button cancelButton = new Button("Cancel");
         cancelButton.setCancelButton(true);
         cancelButton.setOnAction(getCancelButtonEvent());
-        gridPane.add(createButton, 0, 3);
+        gridPane.add(numberFilterButton, 0, 3);
         gridPane.add(cancelButton, 1, 3);
+        Button logNameFilterButton = new Button("Create");
+        logNameFilterButton.setOnAction(getLogNameFilterButtonEvent());
+        gridPane.add(logNameFilterButton, 2, 3);
         return gridPane;
     }
 
-    private EventHandler<ActionEvent> getCreateButtonEvent() {
+    private EventHandler<ActionEvent> getLogNameFilterButtonEvent() {
+        return event -> {
+
+            var targets = testSuiteOfTargets.getTestTargets();
+
+            TestSuiteOfTargets filteredTestSuiteOfTargets = new TestSuiteOfTargets();
+            var filteredTargets = targets.stream().filter(entry -> entry.getTestcases().stream().
+                    filter(tc -> testTargetName.getText().equals(String.join("", tc.getLogsOfTarget()))).findAny().isPresent()).toList();
+            filteredTestSuiteOfTargets.add(filteredTargets);
+            controller.showTestSuites(filteredTestSuiteOfTargets);
+            this.close();
+
+        };
+    }
+
+    private EventHandler<ActionEvent> getNumberFilterButtonEvent() {
 
         return event -> {
-            if (minCounter.getValue() <= maxCounter.getValue()) {
+            if (minCounter.getValue() < maxCounter.getValue()) {
                 var targets = testSuiteOfTargets.getTestTargets();
-                TestSuiteOfTargets testSuiteOfTargets = new TestSuiteOfTargets();
-                for (int i = minCounter.getValue(); i <= maxCounter.getValue(); i++) {
-                    testSuiteOfTargets.add(targets.get(i - 1));
+                TestSuiteOfTargets filteredTestSuiteOfTargets = new TestSuiteOfTargets();
+                for (int i = minCounter.getValue(); i < maxCounter.getValue(); i++) {
+                    filteredTestSuiteOfTargets.add(targets.get(i - 1));
                 }
-                controller.showTestSuites(testSuiteOfTargets);
+                controller.showTestSuites(filteredTestSuiteOfTargets);
                 this.close();
             }
         };
